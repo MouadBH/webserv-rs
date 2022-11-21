5f25a5cfa637223e0bbd172b85babae46607463e
MouadBH <mouad123b@gmail.com>
2022-11-21 22:52:27 +0100

    validating number of threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+
+        }
+
+        ThreadPool { threads }
     }
 
     pub fn execute<F>(&self, f: F)
