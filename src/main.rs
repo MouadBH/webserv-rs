82054314b9551584363e9c1e0d200efe6048b02f
MouadBH <mouad123b@gmail.com>
2022-11-21 22:40:02 +0100

    create a finite number of threads

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::{
     fs,
+    thread,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream}
 };
+use webserv_rs::ThreadPool;
 
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
-
+    let pool = ThreadPool::new(4);
     for stream in listner.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
